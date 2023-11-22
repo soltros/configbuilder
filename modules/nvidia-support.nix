91a08776cbf7a6ac5737deb76f10f6e46d45888c
soltros <142091676+soltros@users.noreply.github.com>
2023-11-22 10:10:46 -0500

    Update nvidia-support.nix

--- modules/nvidia-support.nix
+++ modules/nvidia-support.nix
@@ -1,24 +1,42 @@
-{ config, pkgs, ... }:
-
+{ config, lib, pkgs, ... }:
 {
-  # Enable the X11 windowing system
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
 
-  # OpenGL configuration
+  # Enable OpenGL
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
   };
 
-  # Nvidia configuration
-  hardware.nvidia.nvidiaSettings = true;
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
 
-  # Modesetting
-  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia = {
 
- #Specify driver version
- package = config.boot.kernelPackages.nvidiaPackages.beta;
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+  };
+  ...
 }
-
