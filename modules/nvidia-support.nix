2a4c00375e6ac673d61fd2b0ee376f25dab24206
soltros <142091676+soltros@users.noreply.github.com>
2023-11-22 10:14:24 -0500

    Update nvidia-support.nix

--- modules/nvidia-support.nix
+++ modules/nvidia-support.nix
@@ -38,5 +38,5 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
   };
-  ...
+
 }
