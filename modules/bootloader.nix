e1f85c4b3eef7566915d1b854a5309f2a545667c
soltros <142091676+soltros@users.noreply.github.com>
2023-11-26 20:49:45 -0500

    Update bootloader.nix

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -8,6 +8,6 @@
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
   boot.loader.grub.configurationLimit = 3;
-  boot.kernelPackages = pkgs.linuxPackages_6_5;
+  boot.kernelPackages = pkgs.linuxPackages;
 }
 
