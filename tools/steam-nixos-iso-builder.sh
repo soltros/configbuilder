4e8438bedaaa11c4661de546e5e72914810416de
soltros <142091676+soltros@users.noreply.github.com>
2023-12-05 04:43:36 -0500

    Update steam-nixos-iso-builder.sh

--- tools/steam-nixos-iso-builder.sh
+++ tools/steam-nixos-iso-builder.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
 
-# Define variables
-JOVIAN_ZIP_URL="https://github.com/Jovian-Experiments/Jovian-NixOS/archive/refs/heads/development.zip"
-JOVIAN_DIR="Jovian-NixOS-development"
-FLAKE_PATH="${JOVIAN_DIR}#nixosConfigurations.example-module.config.system.build.isoImage"
-# Replace 'example-module' with the actual name of the module you want to use for the Live CD
+# Custom NixOS ISO Creation Script
 
-# Download and extract the Jovian-NixOS-development.zip file
-wget $JOVIAN_ZIP_URL -O jovian-nixos.zip
-unzip jovian-nixos.zip
+# Path to the custom hardware configuration file
+CUSTOM_HW_CONFIG="custom_hardware.nix"
 
-# Use the correct name of the directory after extraction
-JOVIAN_DIR="$(find . -maxdepth 1 -type d -name 'Jovian-NixOS-development*' -print -quit)"
-FLAKE_PATH="${JOVIAN_DIR}#nixosConfigurations.example-module.config.system.build.isoImage"
+# Create a custom Nix configuration for the ISO
+CUSTOM_NIX_CONFIG="custom_iso.nix"
+cat > $CUSTOM_NIX_CONFIG <<EOF
+{ config, pkgs, lib, ... }:
 
-# Generate the ISO using the flake
-nix build --no-link "$FLAKE_PATH"
+{
+  imports =
+    [ # Include the custom hardware configuration
+      ./$CUSTOM_HW_CONFIG
+      <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+    ];
 
-# Move the resulting ISO to the current directory
-mv $(find /nix/store -name '*.iso' -print -quit) ./jovian-nixos-livecd.iso
+  # Use the unstable channel
+  nixpkgs.config = {
+    allowUnfree = true;
+    packageOverrides = pkgs: {
+      inherit (import (pkgs.fetchFromGitHub {
+        owner = "NixOS";
+        repo = "nixpkgs";
+        rev = "nixos-unstable";
+        sha256 = "0000000000000000000000000000000000000000000000000000"; # Replace with correct hash
+      }) {});
+    };
+  };
 
-# Output the location of the ISO
-echo "Live CD ISO created at $(pwd)/jovian-nixos-livecd.iso"
+  # Select the latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Other configurations can go here
+  # ...
+}
+EOF
+
+# Build the ISO
+nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=$CUSTOM_NIX_CONFIG
+
+# Notify about the ISO creation
+echo "ISO build complete. The ISO can be found in the /nix/store directory."
