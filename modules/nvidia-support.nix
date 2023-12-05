fbe3994dbf702fef6cfef1f3291e61d8ee01c251
soltros <142091676+soltros@users.noreply.github.com>
2023-12-05 02:08:59 -0500

    Update nvidia-support.nix

--- modules/nvidia-support.nix
+++ modules/nvidia-support.nix
@@ -37,7 +37,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
 }
