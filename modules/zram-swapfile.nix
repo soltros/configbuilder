dbe2e136862a808b640402b1f444072c9f496fde
soltros <142091676+soltros@users.noreply.github.com>
2023-12-06 04:41:15 -0500

    Update zram-swapfile.nix

--- modules/zram-swapfile.nix
+++ modules/zram-swapfile.nix
@@ -1,27 +1,32 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  zramSize = 4096; # Size in MB for a 4GB ZRAM device
+in
 {
-  # Enable ZRAM support
+  # Load the zram kernel module
   boot.kernelModules = [ "zram" ];
-  boot.initrd.availableKernelModules = [ "zram" ];
-  boot.initrd.kernelModules = [ "zram" ];
-  boot.kernelParams = [ "zram.num_devices=1" ];
+  boot.extraModprobeConfig = ''
+    options zram num_devices=1
+  '';
 
-  # Configure ZRAM device
-  systemd.services = {
-    zram-setup = {
-      description = "Set up zram swap device";
-      script = ''
-        # Create a zram device
-        echo ${toString (4 * 1024 * 1024 * 1024)} > /sys/block/zram0/disksize # 4GB
-
-        # Set up the zram device as swap
-        mkswap /dev/zram0
-        swapon /dev/zram0
-      '';
-      wantedBy = [ "multi-user.target" ];
-      after = [ "sysinit.target" ];
-      requires = [ "sysinit.target" ];
-    };
+  # Set up the zram device
+  systemd.services.zram = {
+    description = "Setup ZRAM Swap Device";
+    script = ''
+      # Setting up /dev/zram0
+      echo ${toString (zramSize * 1024 * 1024)} > /sys/block/zram0/disksize
+      mkswap /dev/zram0
+      swapon /dev/zram0
+    '';
+    wantedBy = [ "multi-user.target" ];
+    after = [ "sysinit.target" ];
+    before = [ "swap.target" ];
+    requires = [ "sysinit.target" ];
   };
+
+  # Ensure swap is on
+  swapDevices = [
+    { device = "/dev/zram0"; }
+  ];
 }
