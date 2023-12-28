df7b25d2f6fe21fc2fc2b5dc64078bf563971d92
Derrik Diener <142091676+soltros@users.noreply.github.com>
2023-12-28 07:56:36 -0500

    Update snapd-installer.sh

--- tools/snapd-installer.sh
+++ tools/snapd-installer.sh
@@ -3,8 +3,6 @@
 
 ## Upgrade NixOS to Unstable.
 echo "Running this installer assumes you have do not have snapd.nix from Configbuilder enabled. Your system will not build with this enabled. Please enable snapd.nix once everything is done. "
-xdg-open https://raw.githubusercontent.com/soltros/configbuilder/main/modules/snapd.nix
-xdg-open https://github.com/soltros/configbuilder
 echo " "
 echo "Snap packages for NixOS requires NixOS Unstable. Upgrading...."
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
