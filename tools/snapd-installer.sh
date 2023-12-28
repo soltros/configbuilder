42fc23c65ba147f2d395fea8db631b8004610b2a
Derrik Diener <142091676+soltros@users.noreply.github.com>
2023-12-28 07:16:22 -0500

    Create snapd-installer.sh

---
+++ tools/snapd-installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+## Upgrade NixOS to Unstable.
+echo "Running this installer assumes you have snapd.nix from Configbuilder enabled. "
+echo " "
+echo "Snap packages for NixOS requires NixOS Unstable. Upgrading...."
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+sudo nix-channel --update
+sudo nixos-rebuild switch --upgrade
+
+## Download the Nix-Snapd files
+git clone https://github.com/io12/nix-snapd.git
+
+## Enter the Nix-Snapd dir
+cd nix-snapd/
+
+## Build the module
+sudo nix build
