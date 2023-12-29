88fb1cd8746617a7c0971067223e4bd47eb23f18
Derrik Diener <142091676+soltros@users.noreply.github.com>
2023-12-28 20:40:30 -0500

    Update snapd-installer.sh

--- tools/snapd-installer.sh
+++ tools/snapd-installer.sh
@@ -1,56 +1,58 @@
 #!/bin/bash
 
-# Download the Nix-Snapd files
-git clone https://github.com/io12/nix-snapd.git
+# Function to download the Nix-Snapd files
+download_nix_snapd() {
+    git clone https://github.com/io12/nix-snapd.git
+}
 
-# Upgrade NixOS to Unstable.
-echo "Running this installer assumes you do not have snapd.nix from Configbuilder enabled."
-echo "Ensure you have flake-support.nix enabled to build."
-echo "Snap packages for NixOS require NixOS Unstable. Upgrading..."
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
-sudo nix-channel --update
-sudo nixos-rebuild switch --upgrade
+# Function to upgrade NixOS to Unstable
+upgrade_nixos() {
+    echo "Running this installer assumes you do not have snapd.nix from Configbuilder enabled."
+    echo "Ensure you have flake-support.nix enabled to build."
+    echo "Snap packages for NixOS require NixOS Unstable. Upgrading..."
+    sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+    sudo nix-channel --update
+    sudo nixos-rebuild switch --upgrade
+}
 
-# Ask the user for their hostname
-read -p "Enter the new hostname: " new_hostname
+# Function to set up the flake
+setup_flake() {
+    read -p "Enter the new hostname: " new_hostname
 
-# The flake content
-flake_content='{
-  description = "NixOS configuration";
+    flake_content='{
+      description = "NixOS configuration";
+      ... # (rest of the flake content)
+    }'
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nix-snapd.url = "github:io12/nix-snapd";
-    nix-snapd.inputs.nixpkgs.follows = "nixpkgs";
-  };
+    echo "$flake_content" > $HOME/nix-snapd/flake.nix
 
-  outputs = { nixpkgs, nix-snapd }: {
-    nixosConfigurations.my-hostname = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        nix-snapd.nixosModules.default
-        {
-          services.snap.enable = true;
-        }
-      ];
-    };
-  };
-}'
+    FLAKE_FILE="$HOME/nix-snapd/flake.nix"
+    sed -i "s/my-hostname/$new_hostname/g" "$FLAKE_FILE"
+    echo "flake.nix has been updated with the new hostname: $new_hostname"
+}
 
-# Writing to a file
-echo "$script_content" > $HOME/nix-snapd/flake.nix
+# Function to build the module
+build_module() {
+    cd nix-snapd/
+    nix build
+}
 
-# Path to flake.nix file
-FLAKE_FILE="$HOME/nix-snapd/flake.nix"
+# Menu
+while true; do
+    echo "Select an operation:"
+    echo "1. Download Nix-Snapd"
+    echo "2. Upgrade NixOS to Unstable"
+    echo "3. Setup flake"
+    echo "4. Build module"
+    echo "5. Exit"
+    read -p "Enter your choice [1-5]: " choice
 
-# Replace 'my-hostname' with the user-provided hostname in flake.nix
-sed -i "s/my-hostname/$new_hostname/g" "$FLAKE_FILE"
-
-echo "flake.nix has been updated with the new hostname: $new_hostname"
-
-# Enter the Nix-Snapd directory
-cd nix-snapd/
-
-# Build the module
-
-nix build
+    case $choice in
+        1) download_nix_snapd ;;
+        2) upgrade_nixos ;;
+        3) setup_flake ;;
+        4) build_module ;;
+        5) break ;;
+        *) echo "Invalid option. Please try again." ;;
+    esac
+done
