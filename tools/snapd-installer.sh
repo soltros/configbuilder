1ec92b1840879013f1f8f43e58180be518fd1b86
Derrik Diener <142091676+soltros@users.noreply.github.com>
2023-12-28 20:31:47 -0500

    Update snapd-installer.sh

--- tools/snapd-installer.sh
+++ tools/snapd-installer.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-
-## Upgrade NixOS to Unstable.
-echo "Running this installer assumes you have do not have snapd.nix from Configbuilder enabled. Your system will not build with th>
+# Upgrade NixOS to Unstable.
+echo "Running this installer assumes you do not have snapd.nix from Configbuilder enabled."
 echo "Ensure you have flake-support.nix enabled to build."
-echo "Snap packages for NixOS requires NixOS Unstable. Upgrading...."
+echo "Snap packages for NixOS require NixOS Unstable. Upgrading..."
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 sudo nix-channel --update
 sudo nixos-rebuild switch --upgrade
 
-## Download the Nix-Snapd files
+# Download the Nix-Snapd files
 git clone https://github.com/io12/nix-snapd.git
 
 # Ask the user for their hostname
 read -p "Enter the new hostname: " new_hostname
-
-# Optional: Validate the hostname here
 
 # Path to your flake.nix file
 FLAKE_FILE="$HOME/nix-snapd/flake.nix"
@@ -31,10 +28,9 @@ sed -i "s/my-hostname/$new_hostname/g" "$FLAKE_FILE"
 
 echo "flake.nix has been updated with the new hostname: $new_hostname"
 
-
-## Enter the Nix-Snapd dir
+# Enter the Nix-Snapd directory
 cd nix-snapd/
 
-## Build the module
+# Build the module
+# You might need to specify what to build here
 sudo nix build
-
