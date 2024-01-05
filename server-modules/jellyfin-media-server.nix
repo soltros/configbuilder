8db456bc16242943265877de9203a48ee399cda0
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-01-05 07:52:48 -0500

    Create jellyfin-media-server.nix

---
+++ server-modules/jellyfin-media-server.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  # Include Jellyfin packages
+  environment.systemPackages = with pkgs; [
+    jellyfin
+    jellyfin-web
+    jellyfin-ffmpeg
+  ];
+
+  # Configure Jellyfin service
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+
+    # Uncomment and set the following options as needed
+    # group = "jellyfingroup"; # Group under which Jellyfin runs
+    # package = pkgs.jellyfin; # Specific Jellyfin package to use
+    # user = "jellyfinuser"; # User account under which Jellyfin runs
+  };
+}
