af236741af539b3adb672cb9217d488871b8c9b5
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-01-05 13:13:12 +0000

    Add files via upload

---
+++ configbuilder-server.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "os/exec"
+    "path/filepath"
+
+    "github.com/AlecAivazis/survey/v2"
+)
+
+const githubRepoURL = "https://raw.githubusercontent.com/soltros/configbuilder/main/server-modules/"
+const nixosDir = "/etc/nixos/"
+
+var moduleChoices = []string{
+    "bootloader.nix",
+    "bootloader-mbr.nix",
+    "bootloader-systemdboot.nix",
+    "networking.nix",
+    "podman-support.nix",
+    "tailscale-support.nix",
+    "timezone-localization.nix",
+    "unfree-packages.nix",
+    "user-account.nix",
+    "virtualization-support.nix",
+    "garbagecollection.nix",
+    "basic-server.nix",
+    "ssh-server.nix",
+    "vnc-server.nix",
+    "rdp-server.nix",
+    "plex-media-server.nix",
+    "jellyfin-media-server.nix",
+    "nextcloud-server.nix",
+    "samba-shares.nix",
+    "mergerfs-mounts.nix",
+    "docker-support.nix",
+    "unsecure-packages.nix",
+    "flake-support.nix",
+    "swapfile.nix",
+    "automatic-updater.nix",
+    "nixos-hardened.nix",
+    
+}
+
+func createBackup() {
+    backupDir := filepath.Join(nixosDir, "backup")
+    configFile := filepath.Join(nixosDir, "configuration.nix")
+    backupFile := filepath.Join(backupDir, "configuration.nix.backup")
+
+    // Create backup directory if it doesn't exist
+    if _, err := os.Stat(backupDir); os.IsNotExist(err) {
+        if err := os.Mkdir(backupDir, 0755); err != nil {
+            fmt.Printf("Failed to create backup directory: %s\n", err)
+            return
+        }
+    }
+
+    // Copy current configuration.nix to the backup directory
+    if _, err := os.Stat(configFile); err == nil {
+        cmd := exec.Command("cp", configFile, backupFile)
+        if err := cmd.Run(); err != nil {
+            fmt.Printf("Failed to backup configuration file: %s\n", err)
+            return
+        }
+        fmt.Println("Backup created successfully.")
+    } else {
+        fmt.Println("No existing configuration.nix to backup.")
+    }
+}
+
+func downloadModules(selectedModules []string) {
+    for _, module := range selectedModules {
+        url := githubRepoURL + module
+        outputPath := nixosDir + module
+        fmt.Printf("Downloading %s...\n", module)
+        cmd := exec.Command("wget", url, "-O", outputPath)
+        err := cmd.Run()
+        if err != nil {
+            fmt.Printf("Failed to download %s: %s\n", module, err)
+        }
+    }
+}
+
+func generateConfigurationNix(selectedModules []string) {
+    configFile := nixosDir + "configuration.nix"
+
+    file, err := os.Create(configFile)
+    if err != nil {
+        fmt.Println("Error creating configuration.nix:", err)
+        return
+    }
+    defer file.Close()
+
+    // Updated structure of the configuration.nix file with an empty system packages section
+    basicStructure := `{ config, pkgs, ... }:
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+%s
+    ];
+
+  # Add your custom configurations here
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+  ];
+
+  # This value determines the NixOS release with which your system is to be compatible.
+  # Update it according to your NixOS version.
+  system.stateVersion = "23.11"; # Edit according to your NixOS version
+}
+`
+
+    moduleLines := ""
+    for _, module := range selectedModules {
+        moduleLines += fmt.Sprintf("      ./%s\n", module)
+    }
+
+    configContent := fmt.Sprintf(basicStructure, moduleLines)
+
+    _, err = file.WriteString(configContent)
+    if err != nil {
+        fmt.Println("Error writing to configuration.nix:", err)
+        return
+    }
+
+    fmt.Println("configuration.nix generated.")
+}
+
+
+func mainMenu() {
+    var selectedModules []string
+    prompt := &survey.MultiSelect{
+        Message: "Select Nix Modules:",
+        Options: moduleChoices,
+    }
+    survey.AskOne(prompt, &selectedModules, nil)
+
+    downloadModules(selectedModules)
+    generateConfigurationNix(selectedModules)
+}
+
+func main() {
+    createBackup()
+    mainMenu()
+}
