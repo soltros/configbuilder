726245e23babadcf529ee76a2d42f7b25eb90223
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-01-05 14:18:38 -0500

    Create basic-server-apache.nix

---
+++ server-modules/basic-server-apache.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # System configuration
+  services = {
+    
+    # Add other common server services as required:
+    # Nginx or Apache for web serving, Samba for file sharing,
+    # Postfix for a mail server, Syncthing for file synchronization, etc.
+
+    # Example: Apache
+    services.httpd = {
+      enable = true;
+      # Apache specific configurations
+    };
+
+    # Example: Samba
+    samba = {
+      enable = true;
+      # Samba specific configurations
+    };
+
+    # ... Add any other services you need ...
+  };
+
+  # User management (if needed)
+  users.users = {
+    # Define your users and their settings here
+  };
+
+  # Any additional system-wide settings can be configured here
+}
