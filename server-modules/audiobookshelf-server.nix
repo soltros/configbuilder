4a8e4ed950f62469d665f8fb1e10f80db1248262
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-02-05 21:13:54 -0500

    Update audiobookshelf-server.nix

--- server-modules/audiobookshelf-server.nix
+++ server-modules/audiobookshelf-server.nix
@@ -4,6 +4,7 @@
   # Enable the Audiobookshelf service
   services.audiobookshelf = {
     enable = true;
+    dataDir = "/mnt/storage/audiobookshelf/"; # Set the data directory to /mnt/storage/audiobookshelf/
     host = "0.0.0.0"; # Bind to all network interfaces
     port = 8234;
     # Additional Audiobookshelf service configuration can go here
