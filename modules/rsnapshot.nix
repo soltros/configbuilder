d1fe7607b5c8266dfcb3b5a568db9b49d417c747
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-03-10 03:53:18 -0400

    Create rsnapshot.nix

---
+++ modules/rsnapshot.nix
@@ -0,0 +1,8 @@
+{
+  services.rsnapshot.enable = true;
+  services.rsnapshot.enableManualSnapshot = true;
+  # Add any extra configuration lines you need.
+  #services.rsnapshot.extraConfig = ''
+    # Your extra configuration here
+  '';
+}
