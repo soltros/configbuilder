1e07cb321dba7720b2945cc82df8a3df67574a82
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-03-21 23:50:54 -0400

    Update kde-plasma.nix

--- modules/kde-plasma.nix
+++ modules/kde-plasma.nix
@@ -1,10 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-  imports = [
-    <kde2nix/nixos.nix> # Add this line to import the kde2nix module
-  ];
-
   # Plasma 6
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
