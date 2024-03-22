cb2634cdf30413f3ce16499891d02c15bea16c0c
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-03-21 23:28:45 -0400

    Update kde-plasma.nix

--- modules/kde-plasma.nix
+++ modules/kde-plasma.nix
@@ -9,7 +9,6 @@ in {
   #Plasma 6
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
   services.xserver.displayManager.defaultSession = "plasma";
   services.xserver.displayManager.sddm.wayland.enable = true;
   
