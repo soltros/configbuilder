e99319f13116bcdc36ca8d55d882f76e806bc635
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-03-21 23:14:34 -0400

    Create flake.nix

---
+++ flakes/plasma6/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Plasma 6 beta";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    kde2nix.url = "github:nix-community/kde2nix";
+  };
+
+  outputs = { self, nixpkgs, kde2nix, ... }: {
+    nixosConfigurations.yourHostnameHere = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        kde2nix.nixosModules.plasma6,
+        ./configuration.nix,
+        ./networking.nix,
+        ({ services.xserver.desktopManager.plasma6.enable = true; })
+      ];
+    };
+  }; 
+}
