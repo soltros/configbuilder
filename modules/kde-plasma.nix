41cf870e5fe841843c770bdaca46152313dcdd28
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-03-21 23:00:44 -0400

    Update kde-plasma.nix

--- modules/kde-plasma.nix
+++ modules/kde-plasma.nix
@@ -1,23 +1,19 @@
 { config, pkgs, ... }:
 
 let
-  plasma6-overlay-url = "https://raw.githubusercontent.com/soltros/configbuilder/main/overlays/plasma6-overlay.nix";
-  plasma6-overlay = import (builtins.fetchurl plasma6-overlay-url) { inherit (pkgs) fetchurl; };
+  overlayPath = "plasma6-overlay.nix";
+  plasma6-overlay = import overlayPath { inherit pkgs; };
 in {
   nixpkgs.overlays = [ plasma6-overlay ];
-  
-  # Plasma 6
+
+  #Plasma 6
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
   services.xserver.displayManager.defaultSession = "plasma";
   services.xserver.displayManager.sddm.wayland.enable = true;
   
-  # Exclude KDE Plasma applications if desired
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [  ];
-  
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [ ];
+
   # Additional configurations...
 }
-
-
-
