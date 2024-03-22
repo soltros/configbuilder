2acef3b14b3d6cd4a93c85069751381acf87e18d
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-03-21 22:47:47 -0400

    Update kde-plasma.nix

--- modules/kde-plasma.nix
+++ modules/kde-plasma.nix
@@ -1,17 +1,22 @@
 { config, pkgs, ... }:
 
-{
-  #PLASMA 5
-  #services.xserver.desktopManager.plasma5.enable = true;
-  #services.xserver.displayManager.sddm.enable = true;
-  
-  #PLASMA 6
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.defaultSession = "plasma";
-  services.xserver.displayManager.sddm.wayland.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
-  
-  #General required services
-  services.xserver.enable = true;
+let
+  plasma6-overlay-url = "https://raw.githubusercontent.com/soltros/configbuilder/main/overlays/plasma6-overlay.nix";
+  plasma6-overlay = import (builtins.fetchurl plasma6-overlay-url) { inherit (pkgs) fetchurl; };
+in {
+  nixpkgs.overlays = [ plasma6-overlay ];
 
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+
+  # Assuming you want to enable KDE Plasma (adjust based on actual available options for Plasma 6)
+  services.xserver.desktopManager.plasma5.enable = true;
+  
+  # Exclude KDE Plasma applications if desired
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [  ];
+  
+  # Additional configurations...
 }
+
+
+
