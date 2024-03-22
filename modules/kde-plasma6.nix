292914b2618f123a1676747ef3bd3a338d7bc994
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-03-21 23:55:26 -0400

    Update and rename kde-plasma.nix to kde-plasma6.nix

--- modules/kde-plasma6.nix
+++ modules/kde-plasma6.nix
@@ -6,7 +6,8 @@
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.defaultSession = "plasma";
   services.xserver.displayManager.sddm.wayland.enable = true;
-  
+  services.xserver.desktopManager.plasma6.enable = true;
+
   environment.plasma6.excludePackages = with pkgs.kdePackages; [ ];
 
   # Additional configurations...
