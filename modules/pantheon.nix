e28722e7ebb82371d4c08d1effa904e327608723
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-03-30 19:14:30 -0400

    Update pantheon.nix

--- modules/pantheon.nix
+++ modules/pantheon.nix
@@ -4,5 +4,6 @@
   services.xserver.desktopManager.pantheon.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.enable = true;
+  programs.pantheon-tweaks.enable = true;
 }
 
