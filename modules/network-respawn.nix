ede955dc9c0a556f4dd78384489346c68e4110c5
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-03-31 08:01:27 -0400

    Update network-respawn.nix

--- modules/network-respawn.nix
+++ modules/network-respawn.nix
@@ -1,38 +1,31 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
 
-  # Ensure the systemd services are available
-  options.services.rebootOnResume = lib.mkOption {
-    type = lib.types.bool;
-    default = true;
-    description = ''
-      Whether to reboot tailscaled and NetworkManager services upon resuming from suspend.
-    '';
-  };
+let
+  restartServicesScript = pkgs.writeScript "restart-services.sh" ''
+    #!/usr/bin/env sh
+    ${pkgs.coreutils}/bin/sleep 5
+    ${pkgs.systemd}/bin/systemctl restart tailscaled
+    ${pkgs.systemd}/bin/systemctl restart NetworkManager
+  '';
+in
+{
+  options.services.rebootOnResume = lib.mkEnableOption "Restart tailscaled and NetworkManager services upon resuming from suspend";
 
-  config = lib.mkIf config.services.rebootOnResume {
+  config = lib.mkIf config.services.rebootOnResume.enable {
     systemd.services.rebootOnResume = {
       description = "Restart tailscaled and NetworkManager after resume";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network-online.target", "suspend.target", "hibernate.target", "hybrid-sleep.target" ];
-
-      script = ''
-        # Wait for the system to fully resume
-        ${pkgs.coreutils}/bin/sleep 5
-
-        # Restart the tailscaled service
-        ${pkgs.systemd}/bin/systemctl restart tailscaled
-
-        # Restart the NetworkManager service
-        ${pkgs.systemd}/bin/systemctl restart NetworkManager
-      '';
-
+      script = restartServicesScript;
       path = [ pkgs.coreutils pkgs.systemd ];
-      serviceConfig.Type = "oneshot";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = restartServicesScript;
+      };
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network-online.target" "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
     };
 
-    # Create a systemd target for resume
     systemd.targets.resume = {
-      wantedBy = [ "suspend.target", "hibernate.target", "hybrid-sleep.target" ];
+      wantedBy = [ "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
       before = [ "rebootOnResume.service" ];
     };
 
