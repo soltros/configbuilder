dfb5c5c896da26818861d869edc10c7fa8c11899
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-01 17:29:06 -0400

    Update kde-plasma6.nix

--- modules/kde-plasma6.nix
+++ modules/kde-plasma6.nix
@@ -1,17 +1,27 @@
 { config, pkgs, lib, ... }:
 
+let
+  overlay-unstable = self: super: {
+    # Define your overlay here.
+    # Example:
+    # myPackage = super.myPackage.overrideAttrs (oldAttrs: { ... });
+  };
+in
+
 {
-  # Plasma 6
+  # Include the custom overlay.
+  nixpkgs.overlays = [ overlay-unstable ];
+
+  # Enable the X server and configure the display manager and desktop environment.
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.defaultSession = "plasma";
   services.xserver.displayManager.sddm.wayland.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
 
+  # Configure the environment for Plasma6.
   environment.plasma6.excludePackages = with pkgs.kdePackages; [ ];
-
-  # Resolve the `programs.gnupg.agent.pinentryPackage` duplication error by forcing the use of pinentry-qt
+  
+  # Configure GnuPG's pinentry to use pinentry-qt.
   programs.gnupg.agent.pinentryPackage = lib.mkForce pkgs.pinentry-qt;
-
-  # Additional configurations...
 }
