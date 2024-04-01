6a2d2c183ebc465a12b8b1e0ccb7a809ac9b69fd
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-01 17:33:58 -0400

    Update kde-plasma6.nix

--- modules/kde-plasma6.nix
+++ modules/kde-plasma6.nix
@@ -1,27 +1,31 @@
 { config, pkgs, lib, ... }:
 
-let
-  overlay-unstable = self: super: {
-    # Define your overlay here.
-    # Example:
-    # myPackage = super.myPackage.overrideAttrs (oldAttrs: { ... });
-  };
-in
-
 {
-  # Include the custom overlay.
-  nixpkgs.overlays = [ overlay-unstable ];
+  options = {
+    environment.enablePlasma6 = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable Plasma 6 desktop environment.";
+    };
+  };
 
-  # Enable the X server and configure the display manager and desktop environment.
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.defaultSession = "plasma";
-  services.xserver.displayManager.sddm.wayland.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  config = lib.mkIf config.environment.enablePlasma6 {
+    # Plasma 6
+    nixpkgs.overlays = [ overlay-unstable ];
+                services.xserver.enable = true;
+                services.xserver.displayManager.sddm.enable = true;
+                services.xserver.displayManager.defaultSession = "plasma";
+                services.xserver.displayManager.sddm.wayland.enable = true;
+                services.xserver.desktopManager.plasma6.enable = true;
+                environment.plasma6.excludePackages = with pkgs.kdePackages; [ ];
+                programs.gnupg.agent.pinentryPackage = pkgs.lib.mkForce pkgs.pinentry-qt;
 
-  # Configure the environment for Plasma6.
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [ ];
-  
-  # Configure GnuPG's pinentry to use pinentry-qt.
-  programs.gnupg.agent.pinentryPackage = lib.mkForce pkgs.pinentry-qt;
+    environment.systemPackages = with pkgs; [
+      # Assuming you have packages or customizations specific to Plasma 6
+      # You can add them here
+    ];
+
+    programs.gnupg.agent.pinentryPackage = lib.mkForce pkgs.pinentry-qt;
+    # Additional configurations...
+  };
 }
