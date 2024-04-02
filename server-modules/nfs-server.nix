7a75f58dd7414d26b518ae7670c7473c9c26a827
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-01 22:37:33 -0400

    Update nfs-server.nix

--- server-modules/nfs-server.nix
+++ server-modules/nfs-server.nix
@@ -3,22 +3,35 @@
 with lib;
 
 {
-  # Bind-mount directories to be shared via NFS
-  fileSystems = {
-    "/export/media-server-overflow-1" = {
+  # Configuration for NFS server
+  options = {
+    services.nfs.server.enable = mkEnableOption "Enable NFS server";
+
+    services.nfs.server.exports = mkOption {
+      type = types.lines;
+      default = "";
+      example = literalExample ''
+        /export/media-server-overflow-1  *(rw,fsid=0,no_subtree_check,sync)
+        /export/media-server-overflow-2  *(rw,fsid=0,no_subtree_check,sync)
+      '';
+      description = "NFS exports, specifying directories and permissions.";
+    };
+  };
+
+  config = {
+    # Bind-mount directories to be shared via NFS
+    fileSystems."/export/media-server-overflow-1" = {
       device = "/mnt/storage-3/media-server-overflow-1";
       fsType = "none";
       options = [ "bind" ];
     };
-    "/export/media-server-overflow-2" = {
+    fileSystems."/export/media-server-overflow-2" = {
       device = "/mnt/storage/media-server-overflow-2";
       fsType = "none";
       options = [ "bind" ];
     };
-  };
 
-  # Configuration for the NFS server
-  config = {
+    # Enable NFS server and apply exports configuration
     services.nfs.server.enable = true;
     services.nfs.server.exports = ''
       /export/media-server-overflow-1  *(rw,fsid=0,no_subtree_check,sync)
