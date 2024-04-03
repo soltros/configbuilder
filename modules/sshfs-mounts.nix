0c2eb7fdfb7d340be849c34843381efbf8fa0b95
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-03 04:13:26 -0400

    Add files via upload

--- modules/sshfs-mounts.nix
+++ modules/sshfs-mounts.nix
@@ -3,14 +3,14 @@
 {
   environment.systemPackages = with pkgs; [
     # Ensure sshfs is available globally
-    sshfs
+    #sshfs
   ];
 
   systemd.services = {
     sshfs-laptop-backups = {
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
-      script = "sshfs derrik@nixos-server:/mnt/storage/backups/laptop /home/derrik/Backups/Laptop -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
+      script = "/nix/store/j29pq9clj3rlakc5w4wh7x3mpix6fs5p-system-path/bin/sshfs derrik@nixos-server:/mnt/storage/backups/laptop /home/derrik/Backups/Laptop -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
       serviceConfig = {
         Type = "oneshot";
         RemainAfterExit = true;
@@ -21,7 +21,7 @@
     sshfs-desktop-backups = {
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
-      script = "sshfs derrik@nixos-server:/mnt/storage-3/backups/desktop /home/derrik/Backups/Desktop -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
+      script = "/nix/store/j29pq9clj3rlakc5w4wh7x3mpix6fs5p-system-path/bin/sshfs derrik@nixos-server:/mnt/storage-3/backups/desktop /home/derrik/Backups/Desktop -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
       serviceConfig = {
         Type = "oneshot";
         RemainAfterExit = true;
@@ -32,7 +32,7 @@
     sshfs-syncthing-sync = {
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
-      script = "sshfs derrik@nixos-server:/mnt/storage-3/synthing/sync /home/derrik/Sync -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
+      script = "/nix/store/j29pq9clj3rlakc5w4wh7x3mpix6fs5p-system-path/bin/sshfs derrik@nixos-server:/mnt/storage-3/syncthing/sync /home/derrik/Sync -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
       serviceConfig = {
         Type = "oneshot";
         RemainAfterExit = true;
