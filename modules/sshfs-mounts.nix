5656b2d9697975cc6e86c78d18aa9292bdb6114c
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-07 16:29:23 -0400

    Update sshfs-mounts.nix

--- modules/sshfs-mounts.nix
+++ modules/sshfs-mounts.nix
@@ -3,41 +3,50 @@
 {
   environment.systemPackages = with pkgs; [
     # Ensure sshfs is available globally
-    #sshfs
+    sshfs
   ];
 
   systemd.services = {
     sshfs-laptop-backups = {
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
-      script = "/nix/store/j29pq9clj3rlakc5w4wh7x3mpix6fs5p-system-path/bin/sshfs derrik@nixos-server:/mnt/storage/backups/laptop /home/derrik/Backups/Laptop -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
+      requires = [ "network-online.target" ];
       serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStop = "/run/current-system/sw/bin/umount /home/derrik/Backups/Laptop";
+        Type = "forking";
         User = "derrik";
+        ExecStart = "${pkgs.sshfs}/bin/sshfs derrik@nixos-server:/mnt/storage/backups/laptop /home/derrik/Backups/Laptop -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
+        ExecStop = "${pkgs.coreutils}/bin/umount /home/derrik/Backups/Laptop";
+        RemainAfterExit = true;
+        Restart = "on-failure";
+        RestartSec = 5;
       };
     };
     sshfs-desktop-backups = {
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
-      script = "/nix/store/j29pq9clj3rlakc5w4wh7x3mpix6fs5p-system-path/bin/sshfs derrik@nixos-server:/mnt/storage-3/backups/desktop /home/derrik/Backups/Desktop -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
+      requires = [ "network-online.target" ];
       serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStop = "/run/current-system/sw/bin/umount /home/derrik/Backups/Desktop";
+        Type = "forking";
         User = "derrik";
+        ExecStart = "${pkgs.sshfs}/bin/sshfs derrik@nixos-server:/mnt/storage-3/backups/desktop /home/derrik/Backups/Desktop -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
+        ExecStop = "${pkgs.coreutils}/bin/umount /home/derrik/Backups/Desktop";
+        RemainAfterExit = true;
+        Restart = "on-failure";
+        RestartSec = 5;
       };
     };
     sshfs-syncthing-sync = {
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
-      script = "/nix/store/j29pq9clj3rlakc5w4wh7x3mpix6fs5p-system-path/bin/sshfs derrik@nixos-server:/mnt/storage-3/syncthing/sync /home/derrik/Sync -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
+      requires = [ "network-online.target" ];
       serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStop = "/run/current-system/sw/bin/umount /home/derrik/Sync";
+        Type = "forking";
         User = "derrik";
+        ExecStart = "${pkgs.sshfs}/bin/sshfs derrik@nixos-server:/mnt/storage-3/syncthing/sync /home/derrik/Sync -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,IdentityFile=/home/derrik/.ssh/id_rsa";
+        ExecStop = "${pkgs.coreutils}/bin/umount /home/derrik/Sync";
+        RemainAfterExit = true;
+        Restart = "on-failure";
+        RestartSec = 5;
       };
     };
   };
