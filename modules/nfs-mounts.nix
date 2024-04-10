f2e393c81375b15133fc0f5ee306692466757a83
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-10 17:05:51 -0400

    Update nfs-mounts.nix

--- modules/nfs-mounts.nix
+++ modules/nfs-mounts.nix
@@ -4,46 +4,46 @@
   # Ensure rpcbind, which is required for NFS, is enabled
   services.rpcbind.enable = true;
 
-  # Define the NFS mounts using systemd.mounts for more granular control
-  # and to leverage systemd's native mechanisms for handling network dependencies
-  systemd.mounts = let
-    # Define common options for NFS mounts to avoid repetition
-    commonMountOptions = {
-      type = "nfs";
-      mountConfig.Options = "noatime,nfsvers=4.2"; # Example options, adjust as needed
-    };
-  in
-  [
-    # Mount configuration for each NFS share
-    (commonMountOptions // {
+  # Directly defining mounts and automounts
+  systemd.mounts = [
+    {
       what = "nixos-server:/export/Sync";
       where = "/mnt/Sync";
-    })
-    (commonMountOptions // {
+      wantedBy = [ "multi-user.target" ];
+      type = "nfs";
+      options = "noatime,nfsvers=4.2";
+    }
+    {
       what = "nixos-server:/export/Desktop-Backup";
       where = "/mnt/Desktop-Backup";
-    })
-    (commonMountOptions // {
+      wantedBy = [ "multi-user.target" ];
+      type = "nfs";
+      options = "noatime,nfsvers=4.2";
+    }
+    {
       what = "nixos-server:/export/Laptop-Backup";
       where = "/mnt/Laptop-Backup";
-    })
+      wantedBy = [ "multi-user.target" ];
+      type = "nfs";
+      options = "noatime,nfsvers=4.2";
+    }
   ];
 
-  # Use systemd.automounts for lazy-mounting if desired
-  # This is useful if you want to ensure the mounts are only attempted when accessed,
-  # which can be beneficial in environments with potentially unstable network connections at boot
   systemd.automounts = [
     {
       where = "/mnt/Sync";
-      automountConfig.TimeoutIdleSec = "600"; # Example, adjust as needed
+      wantedBy = [ "multi-user.target" ];
+      automountConfig.TimeoutIdleSec = "600";
     }
     {
       where = "/mnt/Desktop-Backup";
-      automountConfig.TimeoutIdleSec = "600"; # Example, adjust as needed
+      wantedBy = [ "multi-user.target" ];
+      automountConfig.TimeoutIdleSec = "600";
     }
     {
       where = "/mnt/Laptop-Backup";
-      automountConfig.TimeoutIdleSec = "600"; # Example, adjust as needed
+      wantedBy = [ "multi-user.target" ];
+      automountConfig.TimeoutIdleSec = "600";
     }
-  ].map (opts: opts // { wantedBy = [ "multi-user.target" ]; }); # Ensure they are started at boot
+  ];
 }
