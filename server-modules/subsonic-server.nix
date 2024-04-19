eb546bdaff16c58b742442dce70eb947b0b9a432
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-19 15:18:55 -0400

    Update subsonic-server.nix

--- server-modules/subsonic-server.nix
+++ server-modules/subsonic-server.nix
@@ -1,59 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
-  options.services.subsonic = {
-    enable = lib.mkEnableOption "Subsonic media server";
-    home = lib.mkOption {
-      type = lib.types.path;
-      default = "/var/lib/subsonic";
-      description = "The directory where Subsonic will create files.";
-    };
-    port = lib.mkOption {
-      type = lib.types.ints.u16;
-      default = 4040;
-      description = "The port on which Subsonic will listen for incoming HTTP traffic.";
-    };
-    httpsPort = lib.mkOption {
-      type = lib.types.ints.u16;
-      default = 0;
-      description = "The port on which Subsonic will listen for incoming HTTPS traffic. Set to 0 to disable.";
-    };
-    maxMemory = lib.mkOption {
-      type = lib.types.int;
-      default = 100;
-      description = "The memory limit (max Java heap size) in megabytes.";
-    };
-    defaultMusicFolder = lib.mkOption {
-      type = lib.types.path;
-      default = "/var/music";
-      description = "Configure Subsonic to use this folder for music.";
-    };
-    defaultPodcastFolder = lib.mkOption {
-      type = lib.types.path;
-      default = "/var/music/Podcast";
-      description = "Configure Subsonic to use this folder for Podcasts.";
-    };
-    defaultPlaylistFolder = lib.mkOption {
-      type = lib.types.path;
-      default = "/var/playlists";
-      description = "Configure Subsonic to use this folder for playlists.";
-    };
-    contextPath = lib.mkOption {
-      type = lib.types.str;
-      default = "/";
-      description = "The context path, i.e., the last part of the Subsonic URL.";
-    };
-    listenAddress = lib.mkOption {
-      type = lib.types.str;
-      default = "0.0.0.0";
-      description = "The host name or IP address on which to bind Subsonic.";
-    };
-    transcoders = lib.mkOption {
-      type = lib.types.listOf(lib.types.path);
-      default = [ "${pkgs.ffmpeg}/bin/ffmpeg" ];
-      description = "List of paths to transcoder executables that should be accessible from Subsonic.";
-    };
-  };
+  # No need to define options if they are already declared in the NixOS module
 
   config = lib.mkIf config.services.subsonic.enable {
     users.users.subsonic = {
@@ -62,34 +10,27 @@
       createHome = true;
     };
 
-    networking.firewall.allowedTCPPorts = [ config.services.subsonic.port ] ++ lib.optional (config.services.subsonic.httpsPort != 0) config.services.subsonic.httpsPort;
-    environment.systemPackages = [ pkgs.subsonic ];
-
-    systemd.services.subsonic = {
-      description = "Subsonic Media Server";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        User = "subsonic";
-        ExecStart = let
-          opts = [
-            "--port=${toString config.services.subsonic.port}"
-            "--https-port=${toString config.services.subsonic.httpsPort}"
-            "--max-memory=${toString config.services.subsonic.maxMemory}"
-            "--default-music-folder=${config.services.subsonic.defaultMusicFolder}"
-            "--default-podcast-folder=${config.services.subsonic.defaultPodcastFolder}"
-            "--default-playlist-folder=${config.services.subsonic.defaultPlaylistFolder}"
-            "--context-path=${config.services.subsonic.contextPath}"
-            "--listen-address=${config.services.subsonic.listenAddress}"
-          ];
-        in "${pkgs.subsonic}/bin/subsonic ${lib.concatStringsSep " " opts}";
-        Restart = "always";
-      };
-
-      preStart = lib.concatMapStringsSep "\n" (transcoder: ''
-        mkdir -p ${config.services.subsonic.home}/transcoders;
-        ln -sf ${transcoder} ${config.services.subsonic.home}/transcoders/;
-      '') config.services.subsonic.transcoders;
+    # Directly use the settings to configure Subsonic service
+    services.subsonic = {
+      enable = true;
+      home = "/var/lib/subsonic";
+      port = 4040;
+      httpsPort = 0;  # HTTPS disabled by default, set to another port to enable
+      maxMemory = 100;
+      defaultMusicFolder = "/var/music";
+      defaultPodcastFolder = "/var/music/Podcast";
+      defaultPlaylistFolder = "/var/playlists";
+      contextPath = "/";
+      listenAddress = "0.0.0.0";
+      transcoders = [ "${pkgs.ffmpeg}/bin/ffmpeg" ];  # Adjust if other transcoders are needed
     };
+
+    networking.firewall.allowedTCPPorts = [ config.services.subsonic.port ] ++
+      lib.optional (config.services.subsonic.httpsPort != 0) config.services.subsonic.httpsPort;
+
+    systemd.services.subsonic.preStart = lib.concatMapStringsSep "\n" (transcoder: ''
+      mkdir -p ${config.services.subsonic.home}/transcoders;
+      ln -sf ${transcoder} ${config.services.subsonic.home}/transcoders/;
+    '') config.services.subsonic.transcoders;
   };
 }
