707d80d9bcad08d72f986cc088b11ad6906301ea
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-23 20:57:58 -0400

    Update enlightenment.nix

--- modules/enlightenment.nix
+++ modules/enlightenment.nix
@@ -1,9 +1,15 @@
 { config, pkgs, ... }:
 
 {
-services.xserver.displayManager.lightdm.enable = true;
-services.xserver.desktopManager.enlightenment.enable = true;
-services.acpid.enable = true;
-services.xserver.libinput.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.enlightenment.enable = true;
+  services.acpid.enable = true;
+  services.xserver.libinput.enable = true;
+
+  # Enabling XDG portal and adding a portal implementation
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];  # Adjust based on your desktop environment needs
+  };
 
 }
