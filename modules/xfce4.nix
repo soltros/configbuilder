9b9c401952c1056adf9b8ed124b9365e0cf23604
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-23 21:41:39 -0400

    Update xfce4.nix

--- modules/xfce4.nix
+++ modules/xfce4.nix
@@ -13,12 +13,4 @@
   # Set LightDM as the default display manager
   services.xserver.displayManager.defaultSession = "xfce";
 
-  # Enable Pipewire for handling audio and video (revised configuration)
-  security.rtkit.enable = true; # Ensures real-time priorities for the Pipewire daemon
-  services.pipewire.enable = true;
-  services.pipewire.alsa.enable = true; # Allows ALSA clients to use Pipewire
-  services.pipewire.pulse.enable = true; # Makes Pipewire act as a drop-in replacement for PulseAudio
-
-  # Optional: Network Manager for network management
-  networking.networkmanager.enable = true;
 }
