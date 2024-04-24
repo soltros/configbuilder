ea9d26225495bea3d86dd4932d7cffe3d580c65c
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-23 21:39:23 -0400

    Update xfce4.nix

--- modules/xfce4.nix
+++ modules/xfce4.nix
@@ -1,7 +1,24 @@
 { config, pkgs, ... }:
 
 {
-  services.xserver.desktopManager.xfce.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
-}
+  # Enable the X11 windowing system
+  services.xserver.enable = true;
 
+  # Configure the display manager
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm.greeters.gtk.enable = true;
+
+  # Enable the XFCE desktop environment
+  services.xserver.desktopManager.xfce.enable = true;
+
+  # Set LightDM as the default display manager
+  services.xserver.displayManager.defaultSession = "xfce";
+
+  # Enable Pipewire for audio and video support
+  security.rtkit.enable = true; # Real-time privileges for Pipewire
+  services.pipewire.enable = true;
+  services.pipewire.mediaSession.enable = true;
+  services.pipewire.alsa.enable = true; # Compatibility with ALSA applications
+  services.pipewire.pulse.enable = true; # To replace PulseAudio functionalities
+
+}
