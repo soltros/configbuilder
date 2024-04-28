93c55a67bbd0c9816883d64b6e600d09b8a5255f
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-27 23:29:23 -0400

    Update derriks-flatpaks.nix

--- modules/derriks-flatpaks.nix
+++ modules/derriks-flatpaks.nix
@@ -1,43 +1,20 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
-let
-  cfg = config.services.myFlatpaks;
-in
 {
-  options.services.myFlatpaks = {
-    enable = lib.mkEnableOption "Custom Flatpak management";
-    flatpakList = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = [ "io.kopia.KopiaUI" "com.slack.Slack" "com.valvesoftware.Steam" ];  # Default list of Flatpaks
-      example = [ "io.kopia.KopiaUI" "com.slack.Slack" "com.valvesoftware.Steam" ];
-      description = "List of Flatpak applications to install from Flathub.";
-    };
-  };
+  system.activationScripts.flatpak-setup = {
+    text = ''
+      # Add the Flathub repository, if it's not already added
+      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-  config = lib.mkIf cfg.enable {
-    programs.flatpak.enable = true;
-
-    environment.systemPackages = with pkgs; [
-      (writeScriptBin "install-flatpaks" ''
-        #!/bin/sh
-        # Add Flathub if it's not already registered
-        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-        # Install applications
-        for app in ${lib.concatStringsSep " " cfg.flatpakList}; do
-          flatpak install flathub $app -y
-        done
-      '')
-    ];
-
-    systemd.services.install-flatpaks = {
-      description = "Install Flatpak applications";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = "${pkgs.writeScriptBin "install-flatpaks" ''...''}/bin/install-flatpaks";
-      };
-    };
+      # Install or update specific flatpak applications
+      ${pkgs.flatpak}/bin/flatpak install -y flathub \
+        io.kopia.KopiaUI \
+        com.valvesoftware.Steam \
+        net.davidotek.pupgui2 \
+        com.heroicgameslauncher.hgl \
+        com.usebottles.bottles \
+        io.github.prateekmedia.appimagepool
+    '';
+    phasen = "setup";
   };
 }
