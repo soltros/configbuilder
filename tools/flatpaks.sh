5310ded0204c45a46cc88c55a06172afc096c551
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-27 23:36:27 -0400

    Update flatpaks.sh

--- tools/flatpaks.sh
+++ tools/flatpaks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+/run/current-system/sw/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak install io.kopia.KopiaUI com.valvesoftware.Steam net.davidotek.pupgui2 com.heroicgameslauncher.hgl com.usebottles.bottles io.github.prateekmedia.appimagepool -y
+/run/current-system/sw/bin/flatpak install io.kopia.KopiaUI com.valvesoftware.Steam net.davidotek.pupgui2 com.heroicgameslauncher.hgl com.usebottles.bottles io.github.prateekmedia.appimagepool -y
