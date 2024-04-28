6802ded773ac6ad9721c2e1b467b8f01b026356b
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-27 23:32:41 -0400

    Update derriks-flatpaks.nix

--- modules/derriks-flatpaks.nix
+++ modules/derriks-flatpaks.nix
@@ -1,20 +1,12 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  system.activationScripts.flatpak-setup = {
-    text = ''
-      # Add the Flathub repository, if it's not already added
-      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-      # Install or update specific flatpak applications
-      ${pkgs.flatpak}/bin/flatpak install -y flathub \
-        io.kopia.KopiaUI \
-        com.valvesoftware.Steam \
-        net.davidotek.pupgui2 \
-        com.heroicgameslauncher.hgl \
-        com.usebottles.bottles \
-        io.github.prateekmedia.appimagepool
+  system.activationScripts.flatpaks = {
+    text = ''
+      # Fetch and execute the external shell script
+      ${pkgs.curl}/bin/curl -sS https://raw.githubusercontent.com/soltros/configbuilder/main/tools/flatpaks.sh | ${pkgs.bash}/bin/bash
     '';
-    phasen = "setup";
+    phase = "setup";
   };
 }
