59e9ab9c4e81cb37d8984059f244970df9ef5a55
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-05-03 14:26:09 -0400

    Create flake.nix

---
+++ flakes/flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-cosmic }: {
+    nixosConfigurations = {
+      # NOTE: change "host" to your system's hostname
+      b450m-d3sh = nixpkgs.lib.nixosSystem {
+        modules = [
+          {
+            nix.settings = {
+              substituters = [ "https://cosmic.cachix.org/" ];
+              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+            };
+          }
+          nixos-cosmic.nixosModules.default
+          ./etc/nixos/configuration.nix
+        ];
+      };
+    };
+  };
+}
