2f5c0b8c2c376b599320b5204d19c91aeeaf0939
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-05-14 19:03:25 -0400

    Update zram-swapfile.nix

--- modules/zram-swapfile.nix
+++ modules/zram-swapfile.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 let
-  zramSize = 4096; # Size in MB for a 4GB ZRAM device
+  zramSizeMB = 4096; # Size in MB for a 4GB ZRAM device
 in
 {
   # Load the zram kernel module
@@ -13,16 +13,18 @@ in
   # Set up the zram device
   systemd.services.zram = {
     description = "Setup ZRAM Swap Device";
-    script = ''
-      # Setting up /dev/zram0
-      echo ${toString (zramSize * 1024 * 1024)} > /sys/block/zram0/disksize
-      mkswap /dev/zram0
-      swapon /dev/zram0
-    '';
-    wantedBy = [ "multi-user.target" ];
     after = [ "sysinit.target" ];
     before = [ "swap.target" ];
-    requires = [ "sysinit.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = ''
+        echo ${toString (zramSizeMB * 1024 * 1024)} > /sys/block/zram0/disksize
+        mkswap /dev/zram0
+        swapon /dev/zram0
+      '';
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
   };
 
   # Ensure swap is on
