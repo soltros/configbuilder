3866071bb0f059e12b8e6a092d45b94317faba6f
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-05-24 06:25:34 -0400

    Update nixos-convert.sh

--- tools/nixos-convert.sh
+++ tools/nixos-convert.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+# More info at: https://github.com/elitak/nixos-infect
+
 set -e -o pipefail
 
 autodetectProvider() {
@@ -18,6 +20,7 @@ makeConf() {
     return 0
   fi
 
+  # NB <<"EOF" quotes / $ ` in heredocs, <<EOF does not
   mkdir -p /etc/nixos
   local network_import=""
 
@@ -34,10 +37,22 @@ makeConf() {
   zramSwap.enable = ${zramswap};
   networking.hostName = "$(hostname -s)";
   networking.domain = "$(hostname -d)";
-  services.openssh.enable = false;
-  users.users.root = {
-    initialPassword = "nixos";
+  services.openssh.enable = true;
+  users.users.root.openssh.authorizedKeys.keys = [$(while read -r line; do
+    line=$(echo -n "$line" | sed 's/\r//g')
+    trimmed_line=$(echo -n "$line" | xargs)
+    echo -n "''$trimmed_line'' "
+  done <<< "$keys")];
+
+  # Create a default user named "nixos" with password "nixos"
+  users.users.nixos = {
+    isNormalUser = true;
+    home = "/home/nixos";
+    extraGroups = ["wheel"];  # Allow the user to use sudo
+    openssh.authorizedKeys.keys = [];
+    password = "nixos";
   };
+
   system.stateVersion = "23.11";
 }
 EOF
@@ -64,6 +79,7 @@ EOF
     availableKernelModules+=('"vmw_pvscsi"')
   fi
 
+  # If you rerun this later, be sure to prune the filesSystems attr
   cat > /etc/nixos/hardware-configuration.nix << EOF
 { modulesPath, ... }:
 {
@@ -79,6 +95,7 @@ EOF
   [[ -n "$doNetConf" ]] && makeNetworkingConf || true
 }
 
+
 makeLightsailConf() {
   mkdir -p /etc/nixos
   cat > /etc/nixos/configuration.nix << EOF
@@ -91,6 +108,7 @@ EOF
 }
 
 makeNetworkingConf() {
+  # XXX It'd be better if we used procfs for all this...
   local IFS=$'\n'
   eth0_name=$(ip address show | grep '^2:' | awk -F': ' '{print $2}')
   eth0_ip4s=$(ip address show dev "$eth0_name" | grep 'inet ' | sed -r 's|.*inet ([0-9.]+)/([0-9]+).*|{ address="\1"; prefixLength=\2; }|')
@@ -133,6 +151,8 @@ EOF
   fi
   cat > /etc/nixos/networking.nix << EOF
 { lib, ... }: {
+  # This file was populated at runtime with the networking
+  # details gathered from the active system.
   networking = {
     nameservers = [ ${nameservers[@]} ];
     defaultGateway = "${gateway}";
@@ -207,30 +227,186 @@ findESP() {
       && esp="$(df "$d" --output=source | sed 1d)" \
       && break
   done
-  [[ -z "$esp" ]] && return 1
-  if [[ "$esp" == /dev/* ]]; then
-    espfs=$(lsblk --noheadings --raw --output FSTYPE "$esp")
+  [[ -z "$esp" ]] && { echo "ERROR: No ESP mount point found"; return 1; }
+  for uuid in /dev/disk/by-uuid/*; do
+    [[ $(readlink -f "$uuid") == "$esp" ]] && echo $uuid && return 0
+  done
+}
+
+prepareEnv() {
+  # $esp and $grubdev are used in makeConf()
+  if isEFI; then
+    esp="$(findESP)"
   else
-    espfs=$(df --output=fstype "$esp" | sed 1d)
+    for grubdev in /dev/vda /dev/sda /dev/xvda /dev/nvme0n1 ; do [[ -e $grubdev ]] && break; done
   fi
-  [[ "$espfs" != vfat ]] && return 1
-  return 0
+
+  # Retrieve root fs block device
+  #                   (get root mount)  (get partition or logical volume)
+  rootfsdev=$(mount | grep "on / type" | awk '{print $1;}')
+  rootfstype=$(df $rootfsdev --output=fstype | sed 1d)
+
+  # DigitalOcean doesn't seem to set USER while running user data
+  export USER="root"
+  export HOME="/root"
+
+  # Nix installer tries to use sudo regardless of whether we're already uid 0
+  #which sudo || { sudo() { eval "$@"; }; export -f sudo; }
+  # shellcheck disable=SC2174
+  mkdir -p -m 0755 /nix
 }
 
-generateNixOS() {
+fakeCurlUsingWget() {
+  # Use adapted wget if curl is missing
+  which wget && { \
+    curl() {
+      eval "wget $(
+        (local isStdout=1
+        for arg in "$@"; do
+          case "$arg" in
+            "-o")
+              echo "-O";
+              isStdout=0
+              ;;
+            "-O")
+              isStdout=0
+              ;;
+            "-L")
+              ;;
+            *)
+              echo "$arg"
+              ;;
+          esac
+        done;
+        [[ $isStdout -eq 1 ]] && echo "-O-"
+        )| tr '\n' ' '
+      )"
+    }; export -f curl; }
+}
+
+req() {
+  type "$1" > /dev/null 2>&1 || which "$1" > /dev/null 2>&1
+}
+
+checkEnv() {
+  [[ "$(whoami)" == "root" ]] || { echo "ERROR: Must run as root"; return 1; }
+
+  # Perform some easy fixups before checking
+  # TODO prevent multiple calls to apt-get update
+  (which dnf && dnf install -y perl-Digest-SHA) || true # Fedora 24
+  which bzcat || (which yum && yum install -y bzip2) \
+              || (which apt-get && apt-get update && apt-get install -y bzip2) \
+              || true
+  which xzcat || (which yum && yum install -y xz-utils) \
+              || (which apt-get && apt-get update && apt-get install -y xz-utils) \
+              || true
+  which curl  || fakeCurlUsingWget \
+              || (which apt-get && apt-get update && apt-get install -y curl) \
+              || true
+
+  req curl || req wget || { echo "ERROR: Missing both curl and wget";  return 1; }
+  req bzcat            || { echo "ERROR: Missing bzcat";               return 1; }
+  req xzcat            || { echo "ERROR: Missing xzcat";               return 1; }
+  req groupadd         || { echo "ERROR: Missing groupadd";            return 1; }
+  req useradd          || { echo "ERROR: Missing useradd";             return 1; }
+  req ip               || { echo "ERROR: Missing ip";                  return 1; }
+  req awk              || { echo "ERROR: Missing awk";                 return 1; }
+  req cut || req df    || { echo "ERROR: Missing coreutils (cut, df)"; return 1; }
+
+  # On some versions of Oracle Linux these have the wrong permissions,
+  # which stops sshd from starting when NixOS boots
+  chmod 600 /etc/ssh/ssh_host_*_key
+}
+
+infect() {
+  # Add nix build users
+  # FIXME run only if necessary, rather than defaulting true
+  groupadd nixbld -g 30000 || true
+  for i in {1..10}; do
+    useradd -c "Nix build user $i" -d /var/empty -g nixbld -G nixbld -M -N -r -s "$(which nologin)" "nixbld$i" || true
+  done
+  # TODO use addgroup and adduser as fallbacks
+  #addgroup nixbld -g 30000 || true
+  #for i in {1..10}; do adduser -DH -G nixbld nixbld$i || true; done
+  NIX_INSTALL_URL="${NIX_INSTALL_URL:-https://nixos.org/nix/install}"
+  curl -L "${NIX_INSTALL_URL}" | sh -s -- --no-channel-add
+
+  # shellcheck disable=SC1090
+  source ~/.nix-profile/etc/profile.d/nix.sh
+
+  [[ -z "$NIX_CHANNEL" ]] && NIX_CHANNEL="nixos-23.05"
+  nix-channel --remove nixpkgs
+  nix-channel --add "https://nixos.org/channels/$NIX_CHANNEL" nixos
+  nix-channel --update
+
+  if [[ $NIXOS_CONFIG = http* ]]
+  then
+    curl $NIXOS_CONFIG -o /etc/nixos/configuration.nix
+    unset NIXOS_CONFIG
+  fi
+
+  export NIXOS_CONFIG="${NIXOS_CONFIG:-/etc/nixos/configuration.nix}"
+
+  nix-env --set \
+    -I nixpkgs=$(realpath $HOME/.nix-defexpr/channels/nixos) \
+    -f '<nixpkgs/nixos>' \
+    -p /nix/var/nix/profiles/system \
+    -A system
+
+  # Remove nix installed with curl | bash
+  rm -fv /nix/var/nix/profiles/default*
+  /nix/var/nix/profiles/system/sw/bin/nix-collect-garbage
+
+  # Reify resolv.conf
+  [[ -L /etc/resolv.conf ]] && mv -v /etc/resolv.conf /etc/resolv.conf.lnk && cat /etc/resolv.conf.lnk > /etc/resolv.conf
+
+  # Set label of root partition
+  if [ -n "$newrootfslabel" ]; then
+    echo "Setting label of $rootfsdev to $newrootfslabel"
+    e2label "$rootfsdev" "$newrootfslabel"
+  fi
+
+  # Stage the Nix coup d'état
+  touch /etc/NIXOS
+  echo etc/nixos                  >> /etc/NIXOS_LUSTRATE
+  echo etc/resolv.conf            >> /etc/NIXOS_LUSTRATE
+  echo root/.nix-defexpr/channels >> /etc/NIXOS_LUSTRATE
+  (cd / && ls etc/ssh/ssh_host_*_key* || true) >> /etc/NIXOS_LUSTRATE
+
+  rm -rf /boot.bak
+  isEFI && umount "$esp"
+
+  mv -v /boot /boot.bak || { cp -a /boot /boot.bak ; rm -rf /boot/* ; umount /boot ; }
+  if isEFI; then
+    mkdir -p /boot
+    mount "$esp" /boot
+    find /boot -depth ! -path /boot -exec rm -rf {} +
+  fi
+  /nix/var/nix/profiles/system/bin/switch-to-configuration boot
+}
+
+if [ ! -v PROVIDER ]; then
   autodetectProvider
+fi
 
-  if [[ ! "$NO_SWAP" ]]; then
-    makeSwap
-  fi
+[ "$PROVIDER" = "digitalocean" ] && doNetConf=y # digitalocean requires detailed network config to be generated
+[ "$PROVIDER" = "lightsail" ] && newrootfslabel="nixos"
+if [[ "$PROVIDER" = "digitalocean" ]] || [[ "$PROVIDER" = "servarica" ]] || [[ "$PROVIDER" = "hetznercloud" ]]; then
+	doNetConf=y # some providers require detailed network config to be generated
+fi
 
-  makeConf
-
-  if [[ ! "$NO_SWAP" ]]; then
+checkEnv
+prepareEnv
+checkExistingSwap
+if [[ -z "$NO_SWAP" ]]; then
+    makeSwap # smallest (512MB) droplet needs extra memory!
+fi
+makeConf
+infect
+if [[ -z "$NO_SWAP" ]]; then
     removeSwap
-  fi
-}
+fi
 
-echo "nixos-infect script execution started."
-
-generateNixOS
+if [[ -z "$NO_REBOOT" ]]; then
+  reboot
+fi
