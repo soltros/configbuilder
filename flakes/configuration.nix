d7396f52f9d36c66296c624111684500be9d1e7b
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-06-30 18:24:41 -0400

    Update configuration.nix

--- flakes/configuration.nix
+++ flakes/configuration.nix
@@ -8,11 +8,13 @@
      
     ];
 
-  # Add your custom configurations here
+    # Fish Shell support.
+    users.users.derrik.shell = pkgs.fish;
+    programs.fish.enable = true;
 
   # System packages
   environment.systemPackages = with pkgs; [
-    efibootmgr refind git keepassxc bitwarden python3 appimage-run papirus-icon-theme gnome.dconf-editor libreoffice-qt gnome.gnome-tweaks spotify tailscale vlc gimp wget libsForQt5.ghostwriter wine-staging pavucontrol fluffychat distrobox geany thunderbird ntfs3g firefox flatpak mullvad-vpn discord kopia tdesktop flameshot microsoft-edge ollama screen nodejs pipx ncdu nvtop php adapta-gtk-theme mlocate yt-dlp waybar wofi networkmanagerapplet kitty mako swaybg swaylock grim slurp jq wl-clipboard notify-desktop libnotify playerctl pinta okular plex-media-player virt-manager fish nfs-utils nvidiaPackages.stable podman cups openssh python311Packages.pip python312
+    efibootmgr refind git keepassxc bitwarden python3 appimage-run papirus-icon-theme gnome.dconf-editor libreoffice-qt gnome.gnome-tweaks spotify tailscale vlc gimp wget libsForQt5.ghostwriter wine-staging pavucontrol fluffychat distrobox geany thunderbird ntfs3g firefox flatpak mullvad-vpn discord kopia tdesktop flameshot microsoft-edge ollama screen nodejs pipx ncdu nvtop php adapta-gtk-theme mlocate yt-dlp waybar wofi networkmanagerapplet kitty mako swaybg swaylock grim slurp jq wl-clipboard notify-desktop libnotify playerctl pinta okular plex-media-player virt-manager nfs-utils nvidiaPackages.stable podman cups openssh python311Packages.pip python312
   ];
 
   # This value determines the NixOS release with which your system is to be compatible.
