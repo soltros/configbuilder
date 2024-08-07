22ff9a6caf24bfa33b52e641da24078cf2dbbcdd
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-08-06 20:26:04 -0400

    Update nixos-hardened.nix

--- server-modules/nixos-hardened.nix
+++ server-modules/nixos-hardened.nix
@@ -5,16 +5,18 @@
   networking.firewall = {
     enable = true;
     allowPing = false;
-    trustedInterfaces = [ "lo" ]; # Loopback interface
-    allowedTCPPorts = [ 22 ]; # SSH port, adjust as needed
-  };
-
-  # Enable other security features
-  security = {
-    hideProcessInformation = true;
-    hardened = {
-      cpHidePointer = true;
-      malloc = "scudo";
-    };
+    trustedInterfaces = [ "lo" ]; # Loopback interface only
+    allowedTCPPorts = [
+      22  # SSH port
+      32400  # Plex web interface
+      80  # Nextcloud web interface (HTTP)
+      443  # Nextcloud web interface (HTTPS)
+      8096  # Emby web interface
+    ];
+    allowedUDPPorts = [
+      1900  # Plex discovery
+      5353  # Plex network discovery
+      7359  # Emby discovery
+    ];
   };
 }
