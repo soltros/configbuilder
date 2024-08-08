9a69dd4baae4d52943d077ec9cbc3d4102b4bf23
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-08-08 19:06:50 -0400

    Update motd.nix

--- server-modules/motd.nix
+++ server-modules/motd.nix
@@ -1,11 +1,25 @@
 { config, pkgs, ... }:
 
+let
+  motd = pkgs.writeShellScriptBin "motd"
+    ''
+      #! /usr/bin/env bash
+      echo "
+      *Welcome to ${config.networking.hostName}
+      *  This is a NixOS server, running on ${config.boot.kernelPackages.kernel.version}  
+      *  Uptime: $(uptime -p)
+      *  Load average: $(cat /proc/loadavg | awk '{print $1" "$2" "$3}')
+      *  Memory usage: $(free -h | awk 'NR==2 {print $3" "$4}')
+      
+    '';
+in
+
 {
-  users.motd = pkgs.writeShellScript "motd.sh" ''
-    echo "
-    * This is a NixOS server, running on ${config.boot.kernelPackages.kernel.version} 
-    *  Uptime: $(uptime -p)
-    *  Load average: $(cat /proc/loadavg | awk '{print $1" "$2" "$3}')
-    *  Memory usage: $(free -h | awk 'NR==2 {print $3" "$4}')"
+  environment.systemPackages = [
+    motd
+  ];
+
+  programs.fish.interactiveShellInit =  ''
+    motd
   '';
 }
