613673b0f27efbc17a49170049284c5e8bd8cdf1
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-08-08 18:57:39 -0400

    Update motd.nix

--- server-modules/motd.nix
+++ server-modules/motd.nix
@@ -1,13 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  users.motd = ''*************************************************************
-  *                    Welcome to ${config.networking.hostName}                    *
-  *************************************************************
-  *  This is a NixOS server, running on ${config.boot.kernelPackages.kernel.version}  *
+  users.motd = ''
+  *  This is a NixOS server, running on ${config.boot.kernelPackages.kernel.version}
   *  Uptime: $(uptime -p)
   *  Load average: $(cat /proc/loadavg | awk '{print $1" "$2" "$3}')
-  *  Memory usage: $(free -h | awk 'NR==2 {print $3" "$4}')
-  *************************************************************''
+  *  Memory usage: $(free -h | awk 'NR==2 {print $3" "$4}')''
   ;
 }
