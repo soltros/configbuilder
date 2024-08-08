8fbc7ef24c1579ccd5fcba610d4a904696483f77
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-08-08 19:03:35 -0400

    Update motd.nix

--- server-modules/motd.nix
+++ server-modules/motd.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  users.motd = writeShellScript "motd.sh" ''
+  users.motd = pkgs.writeShellScript "motd.sh" ''
     echo "
-    *This is a NixOS server, running on ${config.boot.kernelPackages.kernel.version} 
+    * This is a NixOS server, running on ${config.boot.kernelPackages.kernel.version} 
     *  Uptime: $(uptime -p)
     *  Load average: $(cat /proc/loadavg | awk '{print $1" "$2" "$3}')
     *  Memory usage: $(free -h | awk 'NR==2 {print $3" "$4}')"
