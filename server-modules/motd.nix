55afde13726025a7b061057e2bfcc1479cde825a
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-08-08 19:17:00 -0400

    Update motd.nix

--- server-modules/motd.nix
+++ server-modules/motd.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 
-let
-  motd = pkgs.writeShellScriptBin "motd"
-    ''
+{
+  environment.systemPackages = with pkgs; [
+    (writeShellScriptBin "motd" ''
       #! /usr/bin/env bash
       echo "
       *Welcome to ${config.networking.hostName}
@@ -10,11 +10,14 @@ let
       *  Uptime: $(uptime -p)
       *  Load average: $(cat /proc/loadavg | awk '{print $1" "$2" "$3}')
       *  Memory usage: $(free -h | awk 'NR==2 {print $3" "$4}')
-    '';
-in
-
-{
-  environment.systemPackages = [
-    motd
+      "
+    '')
   ];
+
+  system.extraSystemBuilderCmds = ''
+    echo "Ignoring errors for motd derivations"
+    for file in /nix/store/*-motd.drv /nix/store/*-man-paths.drv /nix/store/*-motd_fish-completions.drv /nix/store/*-system-path.drv /nix/store/*-nixos-system-nixos-*.drv; do
+      touch "$file"
+    done
+  '';
 }
