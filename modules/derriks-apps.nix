ffc0fd7fc234b37b8871c6614b909e9238497d76
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-08-09 17:01:42 -0400

    Update derriks-apps.nix

--- modules/derriks-apps.nix
+++ modules/derriks-apps.nix
@@ -4,9 +4,7 @@
   # Add packages to the system environment
   environment.systemPackages = with pkgs; [
     bitwarden
-    spot
     git
-    keepassxc
     python312
     appimage-run
     papirus-icon-theme
@@ -30,52 +28,26 @@
     appimage-run
     firefox
     flatpak
-    mullvad-vpn
     discord
     kopia
     tdesktop
-    flameshot
-    microsoft-edge
-    ollama
     screen
     nodejs
     pipx
     ncdu
-    nvtop
     python311Packages.pip
     caffeine-ng
     php
     adapta-gtk-theme
     mlocate
     yt-dlp
-    waybar
-    wofi
-    networkmanagerapplet
-    kitty
-    mako
-    swaybg
-    swaylock
-    grim
-    slurp
-    jq
-    wl-clipboard
-    notify-desktop
-    libnotify
-    playerctl
     pamixer
-    swayidle
-    emojione
-    xclip
-    sway-contrib.grimshot
     gthumb
-    xfce.thunar-archive-plugin
-    xfce.thunar-volman
     unzip
     gnome.file-roller
     lxrandr
     pinta
     okular
-    cinnamon.nemo-with-extensions
     plex-media-player
     virt-manager
   ];
