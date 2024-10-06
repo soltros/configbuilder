65914fcb8ed3409fecbcd2962e20ddfe381a4d98
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-10-06 16:50:15 -0400

    Create nixos-user-repository.nix

---
+++ modules/nixos-user-repository.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
+}
