45f876c4471697f04447300ead0d32686d98cf8d
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-11-10 08:52:22 -0500

    Update steam.nix

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,10 +1,21 @@
-{ config, pkgs, ... }:
-
+{ config, pkgs, lib, ... }:
 {
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "steam"
-    "steam-original"
-    "steam-unwrapped"
-    "steam-run"
-  ];
+  nixpkgs.config = {
+    allowUnfree = true;  # This is the simplest approach if you want to allow all unfree packages
+    
+    # Or if you specifically want to allow only Steam-related packages:
+    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+      "steam"
+      "steam-original"
+      "steam-runtime"
+      "steam-run"
+    ];
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 }
