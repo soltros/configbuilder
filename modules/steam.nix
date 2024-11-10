d572a1be50a6ff36eb2da12514c92abdf7340035
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-11-10 08:45:44 -0500

    Create steam.nix

---
+++ modules/steam.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-original"
+    "steam-unwrapped"
+    "steam-run"
+  ];
+}
