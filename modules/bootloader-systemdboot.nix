67c69db4dbd75c504206ddcb5ac375845de012ad
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-11-10 10:48:45 -0500

    Update bootloader-systemdboot.nix

--- modules/bootloader-systemdboot.nix
+++ modules/bootloader-systemdboot.nix
@@ -1,12 +1,24 @@
 { config, pkgs, ... }:
-
 {
-  # Enable systemd-boot as the bootloader
-  boot.loader.systemd-boot.enable = true;
-
-  # Allow the bootloader to modify EFI variables
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Additional configurations can be added here
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    # Enable systemd-boot
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 10;        # Limit the number of configurations kept
+        consoleMode = "max";           # Use maximum available console resolution
+        editor = false;                # Disable boot entry editing for security
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";  # Specify EFI mount point explicitly
+      };
+      timeout = 5;                     # Boot menu timeout in seconds
+    };
+    
+    # Use latest kernel - fixed syntax
+    kernelPackages = pkgs.linuxPackages_zen;  # or try these alternatives:
+    # kernelPackages = pkgs.linuxPackages_latest;
+    # kernelPackages = pkgs.linuxPackages_stable;
+  };
 }
