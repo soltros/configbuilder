78790a389434a2ee85330ba660e6206de474e081
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-11-14 16:48:04 -0500

    Update gnome-shell.nix

--- modules/gnome-shell.nix
+++ modules/gnome-shell.nix
@@ -4,5 +4,13 @@
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.enable = true;
+
+  # Exclude Gnome Programs I don't use.
+  environment.gnome.excludePackages = with pkgs.gnome; [
+   pkgs.gnome-console  #Gnome Console
+   pkgs.gnome.geary    #Gnome Email client
+   pkgs.gnome.totem    #Gnome Video player
+  ];
+  
 }
 
