5f0d154cb2a1518bedd50d47391f93f10876167a
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-11-17 14:47:49 -0500

    Update gnome-shell.nix

--- modules/gnome-shell.nix
+++ modules/gnome-shell.nix
@@ -1,5 +1,4 @@
 { config, pkgs, ... }:
-
 {
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
@@ -7,10 +6,19 @@
 
   # Exclude Gnome Programs I don't use.
   environment.gnome.excludePackages = with pkgs.gnome; [
-   pkgs.gnome-console  #Gnome Console
-   pkgs.gnome.geary    #Gnome Email client
-   pkgs.gnome.totem    #Gnome Video player
+    pkgs.gnome-console  #Gnome Console
+    pkgs.gnome.geary    #Gnome Email client
+    pkgs.gnome.totem    #Gnome Video player
   ];
-  
-}
 
+  # Add systemd service to restart GNOME Shell after resume
+  systemd.user.services.gnome-shell-restart-after-resume = {
+    description = "Restart GNOME Shell after resume";
+    wantedBy = [ "suspend.target" ];
+    after = [ "suspend.target" ];
+    script = ''
+      sleep 2
+      gnome-shell --replace &
+    '';
+  };
+}
