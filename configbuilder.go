f9ea618857cfb1d65da316cba556bc5aa0f947e8
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-11-21 09:49:44 -0500

    Update configbuilder.go

--- configbuilder.go
+++ configbuilder.go
@@ -155,7 +155,7 @@ func generateConfigurationNix(modules []moduleItem) (string, string) {
 
   # This value determines the NixOS release with which your system is to be compatible.
   # Update it according to your NixOS version.
-  system.stateVersion = "24.05"; # Edit according to your NixOS version
+  system.stateVersion = "24.11"; # Edit according to your NixOS version
 }
 `)
 
